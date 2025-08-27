918e7db69e3590a04c9839b26f67bd09a9173c30
gabriel-wav <gabfer.silva@gmail.com>
2025-08-26 21:01:53 -0300

    first commit

---
+++ receitas/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Receita
+
+
+# Create your views here.
+def home(request):
+    return render(request, "receitas/home.html")
+
+
+def receita_detail(request, id):
+    receita = get_object_or_404(Receita, id=id)
+    context = {"receita": receita}
+    return render(request, "receitas/receita_detail.html", context)
+
+
+def pesquisar_receitas(request):
+    query = request.GET.get("q")
+    resultados = []
+
+    if query:
+        resultados = Receita.objects.filter(title__icontains=query)
+
+    context = {"query": query, "resultados": resultados}
+    return render(request, "receitas/pesquisar_receitas.html", context)
